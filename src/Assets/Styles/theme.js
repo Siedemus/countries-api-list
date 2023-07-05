bb8c03531731bac46ac342985cce189d7ba2c033
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-06 00:43:16 +0200

    add lightTheme

--- src/Assets/Styles/theme.js
+++ src/Assets/Styles/theme.js
@@ -7,4 +7,14 @@ const darkTheme = {
   },
 };
 
-export { darkTheme };
+ const lightTheme = {
+  colors: {
+    elements: "hsl(0, 0%, 100%)",
+    background: "hsl(0, 0%, 98%)",
+    text: "hsl(200, 15%, 8%)",
+    input: "hsl(0, 0%, 52%)",
+  },
+};
+
+
+export { darkTheme, lightTheme };
