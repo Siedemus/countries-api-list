c1fd529caa6c1b55c84215965061192ab932ddde
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-05 22:35:18 +0200

    set themprovider and global styles, add navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import { useEffect } from "react";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./Assets/Styles/theme";
+import { GlobalStyles } from "./Assets/Styles/globalStyles";
+import { Navigation } from "./Features/Navigation";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles />
+      <Navigation />
+    </ThemeProvider>
+  );
 }
 
 export default App;
