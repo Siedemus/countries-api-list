33e84eb36a2b4a6ef478af2dd033128371c6c22b
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-06 00:53:56 +0200

    add theme handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./Assets/Styles/theme";
+import { darkTheme, lightTheme } from "./Assets/Styles/theme";
 import { GlobalStyles } from "./Assets/Styles/globalStyles";
 import { Navigation } from "./Features/Navigation";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+import { selectIsDarkMode } from "./Features/Navigation/DarkModeSwitch/darkModeSlice";
 
 function App() {
+  const darkMode = useSelector(selectIsDarkMode);
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
       <Navigation />
     </ThemeProvider>
