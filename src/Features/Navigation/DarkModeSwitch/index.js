9f037de338ea91af2f9a6e5723cba9ce43e64e1e
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-06 00:44:25 +0200

    add onClick effect

--- src/Features/Navigation/DarkModeSwitch/index.js
+++ src/Features/Navigation/DarkModeSwitch/index.js
@@ -1,10 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsDarkMode, switchDarkMode } from "./darkModeSlice";
 import { Button } from "./styled";
 
 export const DarkModeSwitch = () => {
-    return (
-        <Button>
-            <ion-icon name="moon"></ion-icon>
-            Dark Mode
-        </Button>
-    );
+  const darkMode = useSelector(selectIsDarkMode);
+  const dispatch = useDispatch();
+
+  return (
+    <Button onClick={() => dispatch(switchDarkMode())}>
+      <ion-icon name={darkMode ? "moon" : "moon-outline"}></ion-icon>
+      Dark Mode
+    </Button>
+  );
 };
