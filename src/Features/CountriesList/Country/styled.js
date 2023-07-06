e32453f3b673fcee45e73431be222df5bc5bda77
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-06 22:00:15 +0200

    add animation and change sizes

--- src/Features/CountriesList/Country/styled.js
+++ src/Features/CountriesList/Country/styled.js
@@ -1,17 +1,27 @@
 import { styled } from "styled-components";
 
 export const Container = styled.div`
-  width: 300px;
-  max-height: 480px;
-  border-radius: 5px;
+  max-width: 300px;
+  height: 400px;
+  border-radius: 9px;
   background-color: ${({ theme }) => theme.colors.elements};
   box-shadow: 0px 1px 41px -17px rgba(0, 0, 0, 0.75);
+  transition: 0.5s;
+  cursor: pointer;
+
+  &&:hover {
+    transform: scale(1.05);
+  }
+
+  &&:active {
+    transform: scale(1.05);
+  }
 `;
 
 export const Flag = styled.img`
-  border-radius: 5px 5px 0 0;
-  width: 100%;
-  height: 190px;
+  border-radius: 9px 9px 0 0;
+  width: 300px;
+  height: 200px;
 `;
 
 export const Text = styled.p`
