2e9b6e100c7449dcb4e28b6edb321de4b7a5851d
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-06 22:00:32 +0200

    change sizes

--- src/Features/CountriesList/styled.js
+++ src/Features/CountriesList/styled.js
@@ -1,10 +1,9 @@
 import { styled } from "styled-components";
 
 export const CountriesContainer = styled.div`
-  margin: 70px auto;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
-  max-width: 1800px;
-  gap: 90px;
+  justify-content: space-evenly;
+  margin: auto 5px;
+  gap: 70px;
 `;
