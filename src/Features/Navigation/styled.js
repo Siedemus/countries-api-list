49614a91e28a10bfc891b129f575d7ae9f403955
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-06 21:59:55 +0200

    add media querry

--- src/Features/Navigation/styled.js
+++ src/Features/Navigation/styled.js
@@ -7,4 +7,9 @@ export const Nav = styled.nav`
   justify-content: space-between;
   text-align: center;
   box-shadow: 1px -10px 15px 0px rgba(0, 0, 0, 0.75);
+
+  @media (max-width: 700px) {
+    padding: 30px 15px;
+    font-size: 9px;
+  }
 `;
