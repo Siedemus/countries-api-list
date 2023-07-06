441f99104d3af404fbbf31d14ffd5043985b3a9a
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-06 22:00:40 +0200

    change color

--- src/Assets/Styles/theme.js
+++ src/Assets/Styles/theme.js
@@ -12,7 +12,7 @@ const darkTheme = {
     elements: "hsl(0, 0%, 100%)",
     background: "hsl(0, 0%, 98%)",
     text: "hsl(200, 15%, 8%)",
-    input: "hsl(0, 0%, 52%)",
+    input: "hsl(0,0%,100%)",
   },
 };
 
