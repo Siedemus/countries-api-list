1ce82a613a79a041c146795cb5b055d0a9943d1b
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-08 02:32:03 +0200

    move useEffect to app.js

--- src/App.js
+++ src/App.js
@@ -7,18 +7,39 @@ import { selectIsDarkMode } from "./Features/Navigation/DarkModeSwitch/darkModeS
 import { CountriesList } from "./Features/CountriesList";
 import { SearchBar } from "./Features/SearchBar";
 import { Main } from "./styled";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { CountryDetails } from "./Features/CountryDetails";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCountries } from "./Features/CountriesList/countriesSlice";
 
 function App() {
   const darkMode = useSelector(selectIsDarkMode);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, []);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
       <Navigation />
-      <Main>
-        <SearchBar />
-        <CountriesList />
-      </Main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/country/:id" element={<CountryDetails />} />
+          <Route
+            path="/"
+            element={
+              <Main>
+                <SearchBar />
+                <CountriesList />
+              </Main>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
