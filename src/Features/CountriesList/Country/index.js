d89da1cf53fb26b31afd2a99c49a1d2c00f77681
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-08 02:28:44 +0200

    add onclick event

--- src/Features/CountriesList/Country/index.js
+++ src/Features/CountriesList/Country/index.js
@@ -1,22 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import { Header } from "../../../Common/Header/styled";
 import { Container, Description, Flag, Text } from "./styled";
 
-export const Country = ({ flag, name, population, region, capital }) => {
+export const Country = ({ flag, name, population, region, capital, cca3 }) => {
+  const navigate = useNavigate();
   return (
-      <Container>
-        <Flag src={flag} />
-        <Description>
-          <Header as="h3">{name ? name : "No data"}</Header>
-          <Text>
-            <strong>Population:</strong> {population ? population : "No data"}
-          </Text>
-          <Text>
-            <strong>Region:</strong> {region ? region : "No data"}
-          </Text>
-          <Text>
-            <strong>Capital:</strong> {capital ? capital : "No data"}
-          </Text>
-        </Description>
-      </Container>
+    <Container
+      onClick={() => {
+        navigate(`/country/${cca3}`);
+      }}
+    >
+      <Flag src={flag} />
+      <Description>
+        <Header as="h3">{name ? name : "No data"}</Header>
+        <Text>
+          <strong>Population:</strong> {population ? population : "No data"}
+        </Text>
+        <Text>
+          <strong>Region:</strong> {region ? region : "No data"}
+        </Text>
+        <Text>
+          <strong>Capital:</strong> {capital ? capital : "No data"}
+        </Text>
+      </Description>
+    </Container>
   );
 };
